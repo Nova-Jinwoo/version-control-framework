efecde818561f4db0c606f37ed9533ca95fa145d
Nova (Jinwooooo) <nova@wondermove.net>
2025-08-14 17:15:50 +0900

    feat: skeletal orchestration layer modules

---
+++ backend-cms/src/main/java/com/example/backend_cms/modules/orchestration/debug/DebugService.java
@@ -0,0 +1,5 @@
+package com.example.backend_cms.modules.orchestration.debug;
+
+public class DebugService {
+  
+}
---
+++ backend-cms/src/main/java/com/example/backend_cms/modules/orchestration/management/ManagementService.java
@@ -0,0 +1,5 @@
+package com.example.backend_cms.modules.orchestration.management;
+
+public class ManagementService {
+  
+}
